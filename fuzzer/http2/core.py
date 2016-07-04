c61e291feb412ad6f1c7584f11d81e34798af3e6
Artem Smotrakov <artem.smotrakov@gmail.com>
2016-07-04 16:43:02 -0700

    Added a draft of Frame class

--- fuzzer/http2/core.py
+++ fuzzer/http2/core.py
@@ -3,3 +3,14 @@
 # returns a client connection preface sequence
 def getclientpreface():
     return bytearray('PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n', 'ascii')
+
+class Frame:
+
+    def __init__(self, payload, frame_type, flags, stream_id = 0):
+        self.__payload = payload
+        self.__frame_type = frame_type
+        self.__flags = flags
+        self.__stream_id = stream_id
+
+    def bytes(self):
+        raise Exception('Not implemented yet')
