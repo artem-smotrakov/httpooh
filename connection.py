c8d9f03798e1edb7b9bee4d71c10325ffffb6efe
Artem Smotrakov <artem.smotrakov@gmail.com>
2016-07-04 12:13:08 -0700

    Added a simple TCP client

--- connection.py
+++ connection.py
@@ -3,7 +3,37 @@
 import socket
 import time
 
+# TCPClient is a simple TCP client which just wraps socket's methods
 class TCPClient:
+
+    def __init__(self, host, port):
+        self.__host = host
+        self.__port = port
+        self.__connected = False
+
+    def connect(self):
+        self.__log('connect to %s:%d' % (self.__host, self.__port))
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.connect((self.__host, self.__port))
+
+    def send(self, data):
+        if self.__connected is False:
+            self.connect()
+        self.__socket.sendall(data)
+
+    def receive(self, length = 1024):
+        return self.__socket.recv(length)
+
+    def close(self):
+        self.__connected = False
+        self.__socket.close()
+
+    def __log(self, message):
+        print "%s: %s" % (TCPClient.__name__, message)
+
+# StubbornTCPClient makes multiple attempts to connect and send data
+# if an I/O error occured
+class StubbornTCPClient:
 
     def __init__(self, host, port, max_attempts = 5, delay = 3):
         self.__host = host
@@ -29,11 +59,12 @@ class TCPClient:
             attempt += 1
 
             if self.__connected is False or error is True:
-                print 'TCPClient: connect to %s:%d, attempt #%d' % (self.__host, self.__port, attempt)
+                self.__log('connect to %s:%d, attempt #%d'
+                           % (self.__host, self.__port, attempt))
                 try:
                     self.connect()
                 except socket.error as msg:
-                    print 'TCPClient: could not connect: ', msg
+                    self.__log('could not connect: %s' % msg)
                     time.sleep(self.__delay)
                     continue
 
@@ -41,7 +72,7 @@ class TCPClient:
                 self.__socket.sendall(data)
                 break
             except socket.error as msg:
-                print 'TCPClient: could not send data: ', msg
+                self.__log('could not send data: %s' % msg)
                 error = True
                 continue
 
@@ -53,9 +84,12 @@ class TCPClient:
         try:
             return self.__socket.recv(length)
         except socket.error as msg:
-            print 'TCPClient: could not receive data: ', msg
+            self.__log('could not receive data: %s' % msg)
             return None
 
     def close(self):
         self.__connected = False
         self.__socket.close()
+
+    def __log(self, message):
+        print "%s: %s" % (StubbornTCPClient.__name__, message)
