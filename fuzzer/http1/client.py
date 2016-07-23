a63ff55f923dc2dd38b9f4c10a2eca19930123ae
Artem Smotrakov <artem.smotrakov@gmail.com>
2016-07-23 12:50:27 -0700

    Added DumbByteArrayFuzzer and DumbAsciiStringFuzzer

--- fuzzer/http1/client.py
+++ fuzzer/http1/client.py
@@ -1,62 +1,26 @@
 #!/usr/bin/python
 
 import helper
-import random
+from fuzzer.core import DumbAsciiStringFuzzer
 
 # TODO: add comments
 # TODO: create a test which checks that it generates the same requests
 #       for the same original request, seed and ratios
-# TODO: extract string fuzzer from this class, add end_test parameter to it
+# TODO: add end_test parameter
 # TODO: add 'HTTP1 request line only fuzzer'
 class DumbHTTP1RequestFuzzer:
 
     def __init__(self, request, seed = 0, min_ratio = 0.01, max_ratio = 0.05,
                  start_test = 0, ignored_symbols = ('\r', '\n')):
         # TODO: check if parameters are valid
-        self.__start_test = start_test
-        self.__request = request
-        self.__seed = seed
-        self.__min_bytes = round(min_ratio * len(request));
-        self.__max_bytes = round(max_ratio * len(request));
-        self.__ignored_symbols = ignored_symbols
-        self.reset()
+        self.__dumb_ascii_string_fuzzer = DumbAsciiStringFuzzer(
+            request, seed, min_ratio, max_ratio, start_test, ignored_symbols)
 
     def reset(self):
-        self.__test = self.__start_test
-        self.__random = random.Random()
-        self.__random.seed(self.__seed)
-        self.__random_n = random.Random()
-        self.__random_position = random.Random()
-        self.__random_byte = random.Random()
+        self.__dumb_ascii_string_fuzzer.reset()
 
     def next(self):
-        self.debug('next(): test = {0:d}'.format(self.__test))
-        fuzzed = bytearray(self.__request, 'ascii')
-        seed = self.__random.random() + self.__test
-        if self.__min_bytes == self.__max_bytes:
-            n = self.__min_bytes
-        else:
-            self.__random_n.seed(seed)
-            n = self.__random_n.randrange(self.__min_bytes, self.__max_bytes);
-        self.debug('next(): n = {0:d}'.format(n))
-        self.__random_position.seed(seed)
-        self.__random_byte.seed(seed)
-        i = 0
-        while (i < n):
-            pos = self.__random_position.randint(0, len(fuzzed) - 1)
-            if self.isignored(fuzzed[pos]):
-                self.debug('next(): ignore symbol (pos = {0:d})'.format(pos))
-                continue
-            b = self.__random_byte.randint(0, 255)
-            fuzzed[pos] = b
-            i += 1
-        self.__test += 1
-        self.debug('next(): request: \n{0}'
-                   .format(fuzzed.decode('ascii', 'ignore')))
-        return fuzzed
-
-    def isignored(self, symbol):
-        return symbol in self.__ignored_symbols
+        return self.__dumb_ascii_string_fuzzer.next()
 
     def debug(self, message):
         helper.debug(DumbHTTP1RequestFuzzer.__name__, message)
